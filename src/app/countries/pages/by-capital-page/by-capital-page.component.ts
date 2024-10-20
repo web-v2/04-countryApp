41650011db531b6ade62fbdbba15b9410ef965a1
Samir Vergara V <svergara3@misena.edu.co>
2024-10-20 15:44:38 -0500

    Init CountryApp

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CountriesService } from './../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html'
+})
+export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService){}
+
+  searchByCapital( term: string): void{
+    this.countriesService.searchCapital( term )
+      .subscribe( ct => {
+        this.countries = ct
+      });
+  }
+}
